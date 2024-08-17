cd6c1e38d4bb2062323276ef0e16f2c827277676
ishak <ishaksebsib@gmail.com>
2024-08-17 15:02:04 +0300

    [ENH] setup acentrity ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={cn("min-h-screen bg-background font-sans antialiased")}>
+			<body
+				className={cn(
+					"min-h-screen bg-background font-sans antialiased",
+					fontSans.variable,
+				)}
+			>
 				{children}
 			</body>
 		</html>
