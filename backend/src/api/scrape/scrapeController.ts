1af18586a407fdc422c5e05ef057ffd297670eac
ishak <ishaksebsib@gmail.com>
2024-08-18 15:40:13 +0300

    [ENH] site summerization Added

--- backend/src/api/scrape/scrapeController.ts
+++ backend/src/api/scrape/scrapeController.ts
@@ -10,14 +10,17 @@ export const scrapeController = async (
   const { url } = req.body;
 
   try {
-    const textData = await scrapeService(url);
+    const summary = await scrapeService(url);
     const serviceResponse = ServiceResponse.success(
-      "Scraping successful",
-      textData,
+      "Scraping and summarization successful",
+      summary,
     );
     handleServiceResponse(serviceResponse, res);
   } catch (error) {
-    const serviceResponse = ServiceResponse.failure((error as Error).message,null);
+    const serviceResponse = ServiceResponse.failure(
+      (error as Error).message,
+      null,
+    );
     handleServiceResponse(serviceResponse, res);
   }
 };
--- backend/src/api/scrape/scrapeService.ts
+++ backend/src/api/scrape/scrapeService.ts
@@ -1,4 +1,6 @@
 import puppeteer from "puppeteer";
+import { HfInference } from "@huggingface/inference";
+import { env } from "@/common/utils/envConfig";
 
 export const scrapeService = async (url: string): Promise<string> => {
   if (!url) throw new Error("URL is required");
@@ -9,10 +11,36 @@ export const scrapeService = async (url: string): Promise<string> => {
   try {
     await page.goto(url, { waitUntil: "networkidle2" });
     const textContent = await page.evaluate(() => document.body.innerText);
-    return textContent;
+    const summary = await getSummary(textContent, 80);
+
+    return summary;
   } catch (error) {
     throw new Error("Failed to scrape the site: " + (error as Error).message);
   } finally {
     await browser.close();
+  }
+};
+
+export const getSummary = async (
+  text: string,
+  maxOutput: number,
+): Promise<string> => {
+  const HF_ACCESS_TOKEN = env.HF_ACCESS_TOKEN;
+
+  try {
+    const inference = new HfInference(HF_ACCESS_TOKEN);
+
+    const result = await inference.summarization({
+      model: "sshleifer/distilbart-cnn-12-6",
+      inputs: text,
+      parameters: {
+        max_length: maxOutput,
+      },
+    });
+
+    return result.summary_text;
+  } catch (error) {
+    console.error("Failed to generate summary:", (error as Error).message);
+    throw new Error("Failed to generate summary");
   }
 };
